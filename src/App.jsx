084a37ef509a3ff72a480b7df2a0334f73daeff9
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-06-07 13:50:42 +0200

    Adding files into folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 // Importing files
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from './Home';
-import About from "./About";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 import Vans from "./Vans";
-import VanDetail from "./VanDetail";
-import Layout from "./Layout";
-import Dashboard from "./Dashboard";
-import Income from "./Income";
-import Review from "./Review";
+import VanDetail from "./Vans/VanDetail";
+// import Dashboard from "./Host/Dashboard";
+import Income from "./Host/Income";
+import Review from "./Host/Review";
+import Layout from "./Components/Layout";
+import HostLayout from "./Components/HostLayout";
 
 /**
  * Challenge:
@@ -38,9 +39,14 @@ import "./server"
           <Route path = "/about" element = {< About />}/> 
           <Route path = "/vans" element = {< Vans />}/>
           <Route path = "/vans/:id" element = {< VanDetail />}/>
-          <Route path = "/dashboard" element = {< Dashboard />}/>
-          <Route path = "/income" element = {< Income />}/>
-          <Route path = "/review" element = {< Review />}/>
+
+
+
+          <Route path = "/host" element = {< HostLayout />}>
+            <Route path = "/host/income" element = {< Income />}/>
+            
+            <Route path = "/host/review" element = {< Review />}/>
+          </Route>
         </Route>  
       </Routes>
     </BrowserRouter>
